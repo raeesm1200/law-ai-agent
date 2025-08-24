6f96634fcce15653d5bd59c0c3868d66c2c6c0f6
Rais Taimoor <raeesm1200@gmail.com>
2025-08-24 17:58:44 +0500

    Fix overflow issue of sidebar

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
 
 import { cn } from "./utils";
+import "./scroll-area.css";
 
 function ScrollArea({
   className,
@@ -13,12 +14,12 @@ function ScrollArea({
   return (
     <ScrollAreaPrimitive.Root
       data-slot="scroll-area"
-      className={cn("relative", className)}
+      className={cn("relative flex-1 min-h-0", className)}
       {...props}
     >
       <ScrollAreaPrimitive.Viewport
         data-slot="scroll-area-viewport"
-        className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
+        className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1 overflow-y-auto"
       >
         {children}
       </ScrollAreaPrimitive.Viewport>
